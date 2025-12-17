dbbccc627655044e1e618a738b07f5c0af3de36b
AqbilBarakaa <aqbiltilas@gmail.com>
2025-12-17 17:05:38 +0700

    Update file

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-from flask import Flask, render_template, request  # Untuk membuat web app dan menerima input user
-from bm25 import getDataFromJson, organize, retrieve  # Import fungsi utama dari utils.py (data init dan retrieval)
-import os  # Untuk memeriksa keberadaan file
+from flask import Flask, render_template, request  
+from bm25 import getDataFromJson, organize, retrieve 
 
-app = Flask(__name__)  # Inisialisasi aplikasi Flask
+app = Flask(__name__)  
 
-# --- ROUTE UTAMA (HALAMAN BERANDA) ---
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    results = []  # Menampung hasil pencarian
-    query = ""  # Menyimpan query dari pengguna
-
+    results = []  
+    query = ""  
     if request.method == "POST":
-        query = request.form["query"]  # Ambil input query dari form HTML
-        results = retrieve(query)  # Jalankan fungsi retrieve untuk ambil hasil pencarian dengan BM25
-
-    # Render halaman index.html dan kirimkan query serta hasil pencarian ke dalam template
+        query = request.form["query"] 
+        results = retrieve(query)  
     return render_template("index.html", query=query, results=results)
 
-# --- JALANKAN APLIKASI ---
 
 if __name__ == "__main__":
-    # Cek apakah file hasil preprocessing sudah ada, kalau belum lakukan preprocessing
     if not os.path.exists("full_data_processed_FINAL.p"):
-        print("Processing data...")  # Log bahwa data sedang diproses
-        getDataFromJson()  # Membaca dan preprocessing data mentah dari JSON
-        organize()  # Membuat keyword dan inverted index
+        print("Processing data...") 
+        getDataFromJson() 
+        organize() 
 
-    # Jalankan aplikasi Flask dalam mode debug
     app.run(debug=True)
